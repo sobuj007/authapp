d30f36836294b1f3c2ae1327670366841ee40ca6
Md. Sayeed Anwar <anwarsobuj007@gmail.com>
2026-02-27 15:28:33 +0600

    route

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    Route::post('/reigster', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('forget-password', [AuthController::class, 'forgetpassword']);
+    Route::post('reset-password', [AuthController::class, 'resetpassword']);
+    // protected routegroup
+    Route::middleware('auth:sanctum')->group(function () {
+
+        // auth route-group
+        Route::prefix('auth')->group(function () {
+            Route::post('changepassword', [AuthController::class, 'changepassword']);
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+
+
+
+    });
+});
